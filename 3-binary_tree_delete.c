30cc3dd2cb52498d485e806f30dadcf419c9c3d4
Simn-popo <paulin.simon@live.fr>
2025-04-07 15:14:59 +0200

    betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - function that delete 
+ * binary_tree_delete - function that delete
  * an entire binary tree
  * @tree: pointer to the root node
  * Return: pointer or NULL
@@ -9,9 +9,8 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if(tree == NULL)	
+	if (tree == NULL)
 		return;
-	
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 
