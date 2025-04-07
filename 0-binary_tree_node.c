5b16998e9f9aa6e0ee26f5d1edda8e16dc30b996
Simn-popo <paulin.simon@live.fr>
2025-04-07 12:07:11 +0200

    add node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_node - create a new node
  * @parent: pointeur of node
- * @int: value of the new node
+ * @value: value of the new node
  * Return: return pointer of new node or null
  */
 
@@ -11,12 +11,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    if (new_node == NULL)
-	
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+	binary_tree_t *new;
 
-    return (new_node);
+	if (new_node == NULL)
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	free(new);
+	return (new_node);
 }
