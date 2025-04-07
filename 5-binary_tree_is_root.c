843e43b3a35c7e98fcda4d24f3a77f63618ebc10
Simn-popo <paulin.simon@live.fr>
2025-04-07 16:33:12 +0200

    1st tap

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - function tha checks if 
+ * a given node its a root
+ * @node: pointer to the node
+ * Return: 1 if the node is a root, 0 if its null
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return(0);
+
+	if (node->parent == NULL)
+			return (1);
+		else
+			return (0);
+}
