f29930ecf3776903038262c0a6be72cf35a15705
Simn-popo <paulin.simon@live.fr>
2025-04-08 14:31:43 +0200

    betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -4,11 +4,11 @@
  * binary_tree_size - function that mesure the size of a node
  * in a binary tree
  * @tree: pointer of the node to measure the depth
- * Return: 0 if the tree i NULL 
+ * Return: 0 if the tree i NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return(1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
