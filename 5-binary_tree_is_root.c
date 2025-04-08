d233a82100781d8c2bcdfc1eab4f4f8a3584ab6e
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-04-08 15:48:48 +0200

    github is shit

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,18 +1,7 @@
 #include "binary_trees.h"
 
 /**
-<<<<<<< HEAD
-<<<<<<< Updated upstream
  * binary_tree_is_root - function tha checks if 
-=======
- * binary_tree_is_root - function tha checks if
->>>>>>> main
-=======
- * binary_tree_is_root - function tha checks if
-=======
- * binary_tree_is_root - function tha checks if 
->>>>>>> 0eb369e (1st tap)
->>>>>>> Stashed changes
  * a given node its a root
  * @node: pointer to the node
  * Return: 1 if the node is a root, 0 if its null
@@ -21,33 +10,11 @@
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
-<<<<<<< HEAD
-<<<<<<< Updated upstream
 		return(0);
-
-	if (node->parent == NULL)
-			return (1);
-		else
-			return (0);
-=======
-=======
->>>>>>> Stashed changes
-		return (0);
 
 	if (node->parent == NULL)
 		return (1);
 	else
 		return (0);
-<<<<<<< Updated upstream
->>>>>>> main
-=======
-=======
-		return(0);
 
-	if (node->parent == NULL)
-			return (1);
-		else
-			return (0);
->>>>>>> 0eb369e (1st tap)
->>>>>>> Stashed changes
 }
