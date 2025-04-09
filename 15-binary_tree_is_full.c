db799cb11c44edd857ea4e054c5381c9bc6676e1
Simn-popo <paulin.simon@live.fr>
2025-04-09 11:41:13 +0200

    fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,9 +11,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return (
-		1 +
-		 binary_tree_is_full(tree->left) +
-		 binary_tree_is_full(tree->right)
-	);
+	
+	if (tree->right == NULL && tree->left == NULL)
+		return (1);
+	
+	if (tree->right != NULL && tree->left != NULL)
+	return(binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
 }
