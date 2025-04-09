1b98ea0017d1ade8012df308f154d378b6d56cb8
Simn-popo <paulin.simon@live.fr>
2025-04-09 10:02:03 +0200

    nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - function that counts the nodes
+ * whit at least 1 child in a binary tree
+ * @tree: pointer to the root node
+ * Return: tree is NULL return 0
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	return (0);
+
+	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
