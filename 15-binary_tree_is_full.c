31d812f24697a1ff04f671603da81e6b6a08e2f6
Simn-popo <paulin.simon@live.fr>
2025-04-09 10:13:00 +0200

    is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - function that check
+ * if a binary tree is full
+ * @tree: pointer to the root node of the tree to check
+ * Return: if tree is NULL return 0
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (
+		1 +
+		 binary_tree_is_full(tree->left) +
+		 binary_tree_is_full(tree->right)
+	);
+}
