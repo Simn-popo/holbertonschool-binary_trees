52c9e7b3cafcb19583391f11eb0c99b685cc5878
Simn-popo <paulin.simon@live.fr>
2025-04-11 11:24:33 +0200

    .

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,20 +1,5 @@
 #include "binary_trees.h"
-<<<<<<< HEAD
 
-/**
- * binary_tree_uncle - function that finds
- * the uncle of the node
- * @node: pointer to the node to find the uncle
- * Return: NULL
- */
-binary_tree_t binary_tree_uncle(binary_tree_t *node)
-{
-	if (node == NULL || node->parent == NULL || node->parent)
-		return (NULL);
-	if (node->parent->left == node->parent)
-		return (node->parent->parent->right);
-		return (node->parent->parent->left);
-=======
 /**
  * *binary_tree_uncle - Finds the uncle of a node.
  * @node: The pointer to the node to find the sibling.
@@ -33,5 +18,4 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	}
 
 	return (node->parent->parent->left);
->>>>>>> simon
 }
